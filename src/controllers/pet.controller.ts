b8b8698ab9672d23ada12fb11a0cdd6e871db2ab
devartes <anacavalcante731@gmail.com>
2023-07-29 07:48:24 -0300

    fix(pet.controller.ts): fix logic in createPet and removePet functions

--- src/controllers/pet.controller.ts
+++ src/controllers/pet.controller.ts
@@ -11,18 +11,14 @@ export const createPet = async (req: Request, res: Response) => {
 
     const pet = await Pet.create(petData);
 
-    const tutor = await Tutor.findById(tutorId).populate({
-      path: "pets",
-    });
-
-    if (tutor) {
-      tutor.pets.push(pet);
-      await tutor.save();
-    }
+    const tutor = await Tutor.findById(tutorId);
 
     if (!tutor) {
       return res.status(404).json({ message: "Tutor not found" });
     }
+
+    tutor.pets.push(pet);
+    await tutor.save();
 
     res.status(201).json(pet);
   } catch (error) {
@@ -40,11 +36,9 @@ export const updatePet = async (req: Request, res: Response) => {
       return res.status(400).json({ message: "Invalid petId" });
     }
 
-    const updatedPet = await Pet.findByIdAndUpdate(
-      petId,
-      req.body,
-      { new: true }
-    );
+    const updatedPet = await Pet.findByIdAndUpdate(petId, req.body, {
+      new: true,
+    });
 
     if (!updatedPet) {
       return res.status(404).json({ message: "Pet not found" });
@@ -62,11 +56,20 @@ export const updatePet = async (req: Request, res: Response) => {
 
 export const removePet = async (req: Request, res: Response) => {
   try {
-    const { petId, tutorId } = req.params;
-    const pet = await Pet.findOneAndDelete({ _id: petId, tutor: tutorId });
+    const { petId } = req.params;
+
+    const pet = await Pet.findOneAndDelete({ _id: petId });
+
     if (!pet) {
       return res.status(404).json({ message: "Pet not found" });
     }
+
+    const tutor = await Tutor.findOneAndUpdate(
+      { pets: petId },
+      { $pull: { pets: petId } },
+      { new: true }
+    );
+
     res.status(200).json({ message: "Pet removed successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error while removing pet" });
